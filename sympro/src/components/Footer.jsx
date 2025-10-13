b4f486d60db6a7c371297db7bd39e5bb270fa922
tecrcopentgit <abdulhaadhi015@gmail.com>
2025-10-13 20:11:54 +0530

    app.post edited

--- sympro/src/components/Footer.jsx
+++ sympro/src/components/Footer.jsx
@@ -9,7 +9,12 @@ export default function Footer(){
       <footer className="bg-slate-900 border-t border-purple-500/20 py-8 px-4">
         <div className="max-w-7xl mx-auto text-center text-gray-400">
           <p>&copy; 2025 DRIFTIAN . A Symposium by Final Year Students of Computer Science (B.E).</p>
-          <p className="mt-2">Contact: symposium@college.edu | +91 98765 43210</p>
+          <p className="mt-2">
+            Contact:
+            <a href='https://mail.google.com/mail/u/0/#inbox?compose=KnqNxqWgwpPJGFZSTmfbnNKjtvRctsdVfCvwGhcwRmnjZPqxkzpKBgwjXwVXFDsvLCNscqXbVXvQtKfnCjBtXMLWWFNNGhFmfmnFxxQCVJrQSsXsSpfJcvmJXJLtCNghKkZpNKmZDpwXMVCxKSGKr'>driftiancse2k25@gmail.com </a>| 
+          Phone:
+          <a href=''>+91 86102 65146</a>
+          </p>
         </div>
       </footer>
         
